530565173163aa8a6590ba2b993f486a1377ae31
Elizabeth Viera <elizabeth.viera.codes@gmail.com>
2020-07-13 18:10:14 -0700

    call weather API

---
+++ src/functions/weather.js
@@ -0,0 +1,10 @@
+exports.handler = async function(event, context, callback) {
+	const weather_response = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${process.env.WEATHER_API_KEY}`
+	);
+
+	callback(null, {
+		statusCode: 200,
+		body: await weather_response.text()
+	});
+};
